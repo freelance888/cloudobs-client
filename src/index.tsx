bfeea6921a822936b1dd2316548be9acaad512d9
Roman Vanek <rvanek4@gmail.com>
2022-12-12 22:43:04 +0100

    Update default TS offset to 8 secs

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
 
-window["APP_VERSION"] = "1.7.4";
+window["APP_VERSION"] = "1.7.5";
 
 root.render(
 	<Provider store={store}>
