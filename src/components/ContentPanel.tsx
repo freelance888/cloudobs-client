4b834dcd973a23a425b959ce921c424a8ab6cde9
Roman Vanek <rvanek4@gmail.com>
2023-02-24 14:26:02 +0100

    Fix centering

--- src/components/ContentPanel.tsx
+++ src/components/ContentPanel.tsx
@@ -10,6 +10,8 @@ type Props = {
 };
 
 const ContentPanel: React.FC<Props> = (props: Props) => {
+	const centered = props.centered != null ? props.centered : true;
+
 	const renderActions = useCallback(() => {
 		return (
 			(props.mainActions || props.endActions) && (
@@ -22,7 +24,7 @@ const ContentPanel: React.FC<Props> = (props: Props) => {
 	}, [props.mainActions, props.endActions]);
 
 	return (
-		<div className={classNames("content-panel", { "m-auto": props.centered })}>
+		<div className={classNames("content-panel", { "m-auto": centered })}>
 			{props.actionsOnTop && renderActions()}
 			<div className="content-panel__children">{props.children}</div>
 			{!props.actionsOnTop && renderActions()}
